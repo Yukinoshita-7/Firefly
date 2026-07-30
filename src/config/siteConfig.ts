71e53d2389a7db7140cc22a2fd45dd88b8c61f2e
Yukino <858368941@qq.com>
2026-07-30 21:33:57 +0800

    fix:修改站点url

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -12,7 +12,7 @@ export const siteConfig: SiteConfig = {
 	subtitle: "Demo site",
 
 	// 站点 URL
-	site_url: "https://firefly.cuteleaf.cn",
+	site_url: "https://yukino0103.com",
 
 	// 站点描述
 	description:
